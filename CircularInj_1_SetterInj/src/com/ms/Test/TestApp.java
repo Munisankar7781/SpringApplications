5042715e71ced45d84ae59831bf8aa1ed6cf4551
munis <munis@LAPTOP-N1CT4FMT>
2024-04-19 11:38:27 +0530

    CI-setterInjection

--- CircularInj_1_SetterInj/src/com/ms/Test/TestApp.java
+++ CircularInj_1_SetterInj/src/com/ms/Test/TestApp.java
@@ -1,12 +1,9 @@
 package com.ms.Test;
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.ms.beans.A;
 import com.ms.beans.B;
 
 public class TestApp {
-
 	public static void main(String[] args)
 	{
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("com/ms/cfgs/ApplicationContext.xml");
@@ -15,7 +12,5 @@ public class TestApp {
 		System.out.println("=================");
 		B rr1=ctx.getBean("Bclass",B.class);
 		System.out.println(rr1);
-
 	}
-
 }
